2e423621d5b64e699b740b0a30be02c38157118c
chinei567 <chuthamat.bam3646@gmail.com>
2022-10-21 11:06:57 +0700

    update
    ทดสอบอัพเดต

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -2,6 +2,11 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
-        
+        System.out.println("my name is chinei");
+        System.out.println("what your name?");
+        System.out.println("my name is .....");
+        System.out.println("nice to meet you");
+        System.out.println("nice to meet you too");
+        System.out.println("goodBay");
     }
 }
